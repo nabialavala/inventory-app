22c1ffcf399728530e75085121075f0a72669d47
Nabia Lavala <nabialavala@gmail.com>
2026-04-06 14:40:37 -0400

    Added Item class

---
+++ lib/models/item.dart
@@ -0,0 +1,13 @@
+class Item {
+  final String? id;
+  final String name;
+  final int quantity;
+  final double price;
+
+  Item({
+    this.id,
+    required this.name,
+    required this.quantity,
+    required this.price,
+  });
+}
